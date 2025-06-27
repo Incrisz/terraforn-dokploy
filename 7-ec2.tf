8fe21b5378b0a893fe4a34b600462181bdef43c0
incrisz <incrisz4luv@gmail.com>
2025-06-27 08:28:06 +0100

    ssh added

--- 7-ec2.tf
+++ 7-ec2.tf
@@ -8,18 +8,13 @@ resource "aws_instance" "ec2-worker1" {
     vpc_security_group_ids = [ aws_security_group.sg_custom.id ]
     # key_name = "miseuwest2ipalacaprover"
 
-    # user_data = <<-EOF
-    #     #!/bin/bash
-    #     sudo apt update -y
-    #     sudo apt install -y ansible git
+    user_data = <<-EOF
+        #!/bin/bash
+        sudo apt update -y
+        curl -fsSL https://raw.githubusercontent.com/Incrisz/docker-swarm/main/install-ssh.sh | bash
+        curl -sSL https://dokploy.com/install.sh | sh
 
-    #     # Clone the Ansible playbook from GitHub
-    #     git clone https://github.com/Incrisz/Terraform-ansible-iac-HNG.git /home/ubuntu/Terraform-ansible-iac-HNG
-
-    #     # Run the Ansible playbook
-    #     cd /home/ubuntu/Terraform-ansible-iac-HNG/ansible
-    #     ansible-playbook playbook.yml
-    # EOF
+    EOF
 
     # Tag the instance as "ec2-worker1" to differentiate it from other instances.
     tags = {
@@ -27,7 +22,7 @@ resource "aws_instance" "ec2-worker1" {
     }
 }
 
-output "instance_public_ip" {
+output "user_root-pass_1ncrease-server_ip" {
   description = "Public IP address of the EC2 instance"
   value       = aws_instance.ec2-worker1.public_ip
 }
